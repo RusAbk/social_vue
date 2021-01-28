38e6cbccd991472f00edb3e24e4ebf0790cce302
Ruslan <rusabk@yandex.ru>
2021-01-28 20:17:49 +0300

    result of lesson

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,70 @@
 <template>
   <v-app>
     <v-navigation-drawer
-      v-model="nav"
       app
-      absolute
-      temporary
+      color="green"
       dark
-    ></v-navigation-drawer>
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+    >
+      <v-list nav shaped dense> 
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/2.jpg">
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>
+              SocialLink
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Руслан Абкадиров
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider class="my-3"></v-divider>
+
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Главная
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link to="/about">
+          <v-list-item-icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            О нас
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
 
     <!-- комментарий -->
 
     <v-content class="px-12 py-3" app>
       <v-container fluid>
-        <router-view/>
+        <router-view />
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
-    nav: false
+    nav: true,
   }),
 };
 </script>
